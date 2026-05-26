c43afe1f7a38d5d7d4ccbf95d2391babbb7ca8e3
Elena <rlenka2010gmail.com>
2026-05-26 22:20:33 +0200

    AddTask Test

---
+++ tests/playwright/fixtures/base_fixtures.ts
@@ -0,0 +1,20 @@
+import { test as base } from "@playwright/test";
+import {Page} from '@playwright/test';
+import { TaskManager } from "../support/components/TaskManager";
+
+
+type MyFixtures = {
+  task: TaskManager;
+};
+
+const createFixture = <T>(Component: new (page: Page) => T) => {
+return async ({ page }: { page: Page }, use: (fixture: T) => Promise<void>) => {
+await use(new Component(page));
+};
+};
+
+export const test = base.extend<MyFixtures>({
+  task: [createFixture(TaskManager), { scope: 'test' }],
+});
+
+export { expect } from "@playwright/test";
---
+++ tests/playwright/support/components/TaskManager.ts
@@ -0,0 +1,46 @@
+import { Page, Locator } from '@playwright/test';
+
+export type ImportanceOptions = 'High' | 'Medium' | 'Low';
+export type LabelOptions = 'Work' | 'Social' | 'Home' | 'Hobby';
+
+export class TaskManager {
+  readonly page: Page;
+  readonly titleInput: Locator;
+  readonly descriptionInput: Locator;
+  readonly selectImportance: Locator;
+  readonly selectLabel: Locator;
+  readonly addTaskButton: Locator;
+  readonly editButton: Locator;
+  readonly deleteButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.titleInput = page.getByPlaceholder('Task Title');
+    this.descriptionInput = page.getByPlaceholder('Task Description');
+    this.selectImportance = page.locator('div.flex.gap-2 select').first();
+    this.selectLabel = page.locator('div.flex.gap-2 select').nth(1);
+    this.addTaskButton = page.getByRole('button', { name: 'Add Task' });
+    this.editButton = page.getByRole('button', { name: 'Edit' });
+    this.deleteButton = page.getByRole('button', { name: 'Delete' });
+  }
+
+  async addTitle(title: string) {
+    await this.titleInput.fill(title);
+  }
+
+  async addDescription(description: string) {
+    await this.descriptionInput.fill(description);
+  }
+
+  async selectImportanceOption(option: ImportanceOptions) {
+    await this.selectImportance.selectOption(option); 
+  }
+
+  async selectLabelOption(option: LabelOptions) {
+    await this.selectLabel.selectOption(option);
+  }
+
+  async clickAddTask() {
+    await this.addTaskButton.click();
+  }
+}
